cba9d943f601ef3d35a0452564491abd5e0e19af
bshap93 <bshap93@gmail.com>
2017-09-23 16:33:49 -0600

    Get rid of coffeescript

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module QOLTool
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.generators.javascript_engine = :js
   end
 end
