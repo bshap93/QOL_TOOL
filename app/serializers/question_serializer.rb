a05449193148b58bdeeb70f565abcaa5d4fab454
bshap93 <bshap93@gmail.com>
2017-09-29 18:49:03 -0600

    Questionnaire show is jquery dynamic

--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -1,5 +1,5 @@
 class QuestionSerializer < ActiveModel::Serializer
-  attributes :id, :body, :category, :user, :default
+  attributes :id, :body, :category, :user, :questionnaire, :default
   belongs_to :questionnaire
   belongs_to :category
   belongs_to :user
